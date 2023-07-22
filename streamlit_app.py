d106a3a2bdd539c5899e9d10c48a7371af661cf1
Proxyking11 <86861699+Proxyking11@users.noreply.github.com>
2023-07-22 06:17:29 +0530

    Updated Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.header ('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥑🍞Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.header ('🍌🥭Build your own Fruit Smoothie 🥝🍇')
