2d0127bc4bacc30f83adce5b0bc3b0b856225665
Proxyking11 <86861699+Proxyking11@users.noreply.github.com>
2023-07-22 06:14:23 +0530

    updated menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 streamlit.title ('My parent need healthy food')
 streamlit.header ('Breakfast Menu')
-streamlit.text ('Omega 3 & BlueBerry Datmeal')
-streamlit.text ('Oats')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥑🍞Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
