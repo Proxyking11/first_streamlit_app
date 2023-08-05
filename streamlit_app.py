ac3ffcdef424f0c77639b2ee22c42ec747c05957
Proxyking11 <86861699+Proxyking11@users.noreply.github.com>
2023-08-05 23:17:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load List Contains:")
 streamlit.dataframe(my_data_rows)
 
+streamlit.write('Thanks for adding',add_my_fruit)
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
