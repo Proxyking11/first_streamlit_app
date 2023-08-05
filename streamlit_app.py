2237153b76e06cee172f274a73e833f4e474922a
Proxyking11 <86861699+Proxyking11@users.noreply.github.com>
2023-08-05 21:36:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,6 @@ def get_fruityvice_data(this_fruit_choice):
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
-my_data_rowss = my_cur.fetchall()
+my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load List Contains:")
 streamlit.dataframe(my_data_rows)
