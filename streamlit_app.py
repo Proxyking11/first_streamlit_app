19df661aff5ed9d50fb688f652daf0a2ff7541b0
Proxyking11 <86861699+Proxyking11@users.noreply.github.com>
2023-08-05 23:09:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,5 +35,5 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load List Contains:")
-streamlit.text(my_data_rows)
+streamlit.dataframe(my_data_rows)
 
