a365554845255febf938e657aa61220d97a5727e
Proxyking11 <86861699+Proxyking11@users.noreply.github.com>
2023-08-05 23:57:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,6 @@ try:
         back_from_function = get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
 
-
 streamlit.header('The Fruit Load List Contains:')
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
